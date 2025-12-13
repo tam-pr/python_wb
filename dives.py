b329d007019c681e35c3444a6dc882fa7490dc6c
tam-pr <a01648580@tec.mx>
2025-12-12 18:09:46 -0600

    added the weighter func
    added weighter func so its not repeated

--- dives.py
+++ dives.py
@@ -34,169 +34,55 @@ def dive_difficulty():
     return difficulty
 
 
+def weighter(values):
+    probs=np.array(values)
+
+    choice=np.random.choice(7,p=probs/100)
+
+    match choice:
+        case 0:
+            diff=np.arange(1.4,1.9,0.1)
+        case 1: 
+            diff=np.arange(2.0,2.5,0.1)
+        case 2:
+            diff=np.arange(2.5,3.0,0.1)
+        case 3: 
+            diff=np.arange(3.0,3.5,0.1)
+        case 4:
+            diff=np.arange(3.5,4.0,0.1)
+        case 5: 
+            diff=np.arange(4.0,4.5,0.1)
+        case 6: 
+            diff=np.arange(4.5,4.7,0.1)
+
+    difficulty= np.random.choice(diff)
+    return difficulty
+
 def scores(difficulty):
+
     if difficulty >= 1.4 and difficulty <1.9: 
-            probs=np.array([5,10,15,30,20,15,5])
-
-            choice=np.random.choice(7,p=probs/100)
-
-            match choice:
-                case 0:
-                    sco=np.arange(0,2.0,0.1)
-                case 1: 
-                    sco=np.arange(2.0,4.0,0.1)
-                case 2:
-                    sco=np.arange(4.0,5.5,0.1)
-                case 3: 
-                    sco=np.arange(5.5,6.5,0.1)
-                case 4:
-                    sco=np.arange(6.5,8.5,0.1)
-                case 5: 
-                    sco=np.arange(8.5,9.0,0.1)
-                case 6: 
-                    sco=np.arange(9.0,10.0,0.1)
-
-            dive_score= np.random.choice(sco)
+        values=[5,10,15,30,20,15,5]
     
     elif difficulty >= 2.0 and difficulty <2.5:
-            probs=np.array([5,8,15,25,25,17,5])
+        values=[5,8,15,25,25,17,5]
 
-            choice=np.random.choice(7,p=probs/100)
-
-            match choice:
-                case 0:
-                    sco=np.arange(0,2.0,0.1)
-                case 1: 
-                    sco=np.arange(2.0,4.0,0.1)
-                case 2:
-                    sco=np.arange(4.0,5.5,0.1)
-                case 3: 
-                    sco=np.arange(5.5,6.5,0.1)
-                case 4:
-                    sco=np.arange(6.5,8.5,0.1)
-                case 5: 
-                    sco=np.arange(8.5,9.0,0.1)
-                case 6: 
-                    sco=np.arange(9.0,10.0,0.1)
-
-            dive_score= np.random.choice(sco)
-    
     elif difficulty >= 2.5 and difficulty <3.0:
-            probs=np.array([5,8,15,20,30,17,5])
+         values=[5,8,15,20,30,17,5]
 
-            choice=np.random.choice(7,p=probs/100)
-
-            match choice:
-                case 0:
-                    sco=np.arange(0,2.0,0.1)
-                case 1: 
-                    sco=np.arange(2.0,4.0,0.1)
-                case 2:
-                    sco=np.arange(4.0,5.5,0.1)
-                case 3: 
-                    sco=np.arange(5.5,6.5,0.1)
-                case 4:
-                    sco=np.arange(6.5,8.5,0.1)
-                case 5: 
-                    sco=np.arange(8.5,9.0,0.1)
-                case 6: 
-                    sco=np.arange(9.0,10.0,0.1)
-
-            dive_score= np.random.choice(sco)
-    
     elif difficulty >= 3.0 and difficulty <3.5:
-            probs=np.array([5,8,17,20,30,15,5])
-
-            choice=np.random.choice(7,p=probs/100)
-
-            match choice:
-                case 0:
-                    sco=np.arange(0,2.0,0.1)
-                case 1: 
-                    sco=np.arange(2.0,4.0,0.1)
-                case 2:
-                    sco=np.arange(4.0,5.5,0.1)
-                case 3: 
-                    sco=np.arange(5.5,6.5,0.1)
-                case 4:
-                    sco=np.arange(6.5,8.5,0.1)
-                case 5: 
-                    sco=np.arange(8.5,9.0,0.1)
-                case 6: 
-                    sco=np.arange(9.0,10.0,0.1)
-
-            dive_score= np.random.choice(sco)
-
+        values=[5,8,17,20,30,15,5]
+    
     elif difficulty >= 3.5 and difficulty <4.0:
-            probs=np.array([5,8,17,20,30,15,5])
-
-            choice=np.random.choice(7,p=probs/100)
-
-            match choice:
-                case 0:
-                    sco=np.arange(0,2.0,0.1)
-                case 1: 
-                    sco=np.arange(2.0,4.0,0.1)
-                case 2:
-                    sco=np.arange(4.0,5.5,0.1)
-                case 3: 
-                    sco=np.arange(5.5,6.5,0.1)
-                case 4:
-                    sco=np.arange(6.5,8.5,0.1)
-                case 5: 
-                    sco=np.arange(8.5,9.0,0.1)
-                case 6: 
-                    sco=np.arange(9.0,10.0,0.1)
-
-            dive_score= np.random.choice(sco)
+        values=[5,8,17,20,30,15,5]
 
     elif difficulty >= 4.0 and difficulty <4.5:
-            probs=np.array([5,8,17,20,30,15,5])
-
-            choice=np.random.choice(7,p=probs/100)
-
-            match choice:
-                case 0:
-                    sco=np.arange(0,2.0,0.1)
-                case 1: 
-                    sco=np.arange(2.0,4.0,0.1)
-                case 2:
-                    sco=np.arange(4.0,5.5,0.1)
-                case 3: 
-                    sco=np.arange(5.5,6.5,0.1)
-                case 4:
-                    sco=np.arange(6.5,8.5,0.1)
-                case 5: 
-                    sco=np.arange(8.5,9.0,0.1)
-                case 6: 
-                    sco=np.arange(9.0,10.0,0.1)
-
-            dive_score= np.random.choice(sco)
+        values[5,8,17,20,30,15,5]
+    
     else: 
-            probs=np.array([5,8,17,20,30,15,5])
+        values=[5,8,17,20,30,15,5]
 
-            choice=np.random.choice(7,p=probs/100)
-
-            match choice:
-                case 0:
-                    sco=np.arange(0,2.0,0.1)
-                case 1: 
-                    sco=np.arange(2.0,4.0,0.1)
-                case 2:
-                    sco=np.arange(4.0,5.5,0.1)
-                case 3: 
-                    sco=np.arange(5.5,6.5,0.1)
-                case 4:
-                    sco=np.arange(6.5,8.5,0.1)
-                case 5: 
-                    sco=np.arange(8.5,9.0,0.1)
-                case 6: 
-                    sco=np.arange(9.0,10.0,0.1)
-
-            dive_score= np.random.choice(sco)
-
-    return dive_score
-
+    score=weighter(values)
+    return score
 
 
 def main(): 
